5700d27dd304df8ec8d3a602be33a40b70920394
flippinroo2 <flippinroo2@gmx.com>
2021-08-12 07:29:05 -0400

    .

--- contracts/Fuji.sol
+++ contracts/Fuji.sol
@@ -115,32 +115,64 @@ contract Fuji is IERC20 {
     view
     override
     returns (uint256)
-  {}
+  {
+        return _balances[account];}
 
   function transfer(address recipient, uint256 amount)
     external
     override
     returns (bool)
-  {}
+  {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+        }
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+
+        _afterTokenTransfer(sender, recipient, amount);}
 
   function allowance(address owner, address spender)
     external
     view
     override
     returns (uint256)
-  {}
+  {return _allowances[owner][spender]}
 
   function approve(address spender, uint256 amount)
     external
     override
     returns (bool)
-  {}
+  {
+        address owner = _msgSender();
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+        return true;}
 
   function transferFrom(
     address spender,
     address recipient,
     uint256 amount
-  ) external override returns (bool) {}
+  ) external override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
+
+        return true;}
 
   function name() external view returns (string memory) {
     return _name;
