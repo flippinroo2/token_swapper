da02041e21e0c9aa42a17d4a87dafd572d843032
flippinroo2 <flippinroo2@gmx.com>
2021-08-11 21:58:58 -0400

    Got tests working for Truffle by using the "at()" function and specificaly entering the proper contract address.

--- test/00_test_fuji.js
+++ test/00_test_fuji.js
@@ -33,40 +33,58 @@ contract('Fuji', (accounts) => {
     sender = { balance: 0, address: accounts[1] },
     receiver = { balance: 0, address: accounts[2] },
     user = { balance: 0, address: accounts[3] };
-  let fuji, contractAddress;
-  let test;
+  let fuji,
+    fujiMetadata = {},
+    contractAddress;
 
   before(async () => {
     // These functions below only work if hardhat has compiled the abis
     // const temp = await hardhatEthers.getContractFactory('Fuji');
-    // test = await temp.deploy('Fuji', 'FUJI');
+    // const test = await temp.deploy('Fuji', 'FUJI');
 
-    fuji = await Fuji.deployed();
+    // These functions all link to a different version of the Fuji contract that's not on Ganache.
+    // const FujiDefaults = await Fuji.defaults();
+    // const FujiAddress = await Fuji.address;
+    // const fujiNew = await Fuji.new('Fuji', 'FUJI');
+    // const fujiDeployed = await Fuji.deployed();
 
-    contractAddress = fuji.address;
-    console.log('fuji address: %s', contractAddress);
+    fuji = await Fuji.at('0x0eA5fa02b89e2c10c5e1b5CF5A254965007e9DC0');
 
     owner.address = await fuji.owner();
-    console.log('owner address: %s', owner.address);
+
+    fujiMetadata.address = fuji.address;
+    fujiMetadata.owner = owner.address;
+    fujiMetadata.name = await fuji.name();
+    fujiMetadata.symbol = await fuji.symbol();
+    fujiMetadata.decimals = utils.hexToNumber(await fuji.decimals());
+    fujiMetadata.totalSupply = utils.hexToNumber(await fuji.totalSupply());
   });
 
   describe('Deployment', async () => {
     it('DEPLOY', () => {
+      const { address } = fuji;
       assert.equal(owner.address, accounts[0]);
-      assert.notEqual(contractAddress, 0x0);
-      assert.notEqual(contractAddress, '');
-      assert.notEqual(contractAddress, null);
-      assert.notEqual(contractAddress, undefined);
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, '');
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
     });
 
     it('MINT', async () => {
       const mintTransaction = await fuji.mint(owner.address, 10);
+      const { tx, receipt } = mintTransaction;
+      if (DEBUG) {
+        console.log(
+          `Transaction: ${tx}\nFrom: ${receipt.from}\nTo: ${receipt.to}\nBlock #: ${receipt.blockNumber}\nGas: ${receipt.gasUsed}`,
+        );
+      }
       const balanceOfTransaction = await fuji.balanceOf(owner.address);
+      const [balance] = balanceOfTransaction.words;
       owner.balance = utils.hexToNumber(balanceOfTransaction);
       if (DEBUG) {
         debugger;
       }
-      expect(owner.balance).to.equal(10);
+      expect(owner.balance).to.equal(balance);
     });
   });
 
