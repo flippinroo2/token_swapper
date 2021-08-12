41786f4923cd69f149ea4da875e5e3f9cc828fcd
flippinroo2 <flippinroo2@gmx.com>
2021-08-12 10:22:35 -0400

    Changed the Template.sol to 4 spaces.

--- contracts/Template.sol
+++ contracts/Template.sol
@@ -16,113 +16,102 @@ import '@openzeppelin/contracts/utils/Arrays.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 
 contract Template is IERC20 {
-  using Address for address;
-  using SafeMath for uint256;
-  using Strings for string;
+    using Address for address;
+    using SafeMath for uint256;
+    using Strings for string;
 
-  bool private constant DEBUG = false;
+    address private _admin;
+    string private _name;
+    string private _symbol;
+    uint256 private _totalSupply;
+    uint256 public _totalMinted;
+    uint8 public constant TOKEN_DECIMALS = 18;
+    uint8 private constant _NOT_ENTERED = 1;
+    uint8 private constant _ENTERED = 2;
+    uint8 private _status;
 
-  address private _admin;
-  string private _name;
-  string private _symbol;
-  uint256 private _totalSupply;
-  uint256 public _totalMinted;
-  uint8 public constant TOKEN_DECIMALS = 18;
-  uint8 private constant _NOT_ENTERED = 1;
-  uint8 private constant _ENTERED = 2;
-  uint8 private _status;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
-  mapping(address => uint256) private _balances;
-  mapping(address => mapping(address => uint256)) private _allowances;
+    event AdminChanged(address indexed previousAdmin, address indexed newAdmin);
+    event Fallback(address indexed sender, uint256 value);
 
-  event AdminChanged(address indexed previousAdmin, address indexed newAdmin);
-  event Fallback(address indexed sender, uint256 value);
-
-  modifier security {
-    require(msg.sender == _admin, 'Must be contract admin');
-    _;
-  }
-
-  modifier safe(address account) {
-    require(account != address(0), 'Cannot transact with the zero address');
-    _;
-  }
-
-  modifier restricted(uint256 number) {
-    require(number != 0, 'Number cannot be zero');
-    require(number > 0, 'Must be a positive number.');
-    require(number <= _totalSupply, 'Must be less than total supply.');
-    require(
-      (_totalMinted + number) <= _totalSupply,
-      'This would cause the total minted coins to be more than the total supply.'
-    );
-    _;
-  }
-
-  modifier reentrancyProtection() {
-    require(_status != _ENTERED, 'Reentrant call');
-    _status = _ENTERED;
-    _;
-    _status = _NOT_ENTERED;
-  }
-
-  constructor(
-    string memory name_,
-    string memory symbol_
-  ) payable {
-    if (DEBUG) {
-      console.log('Contract creator: %s', msg.sender);
-      console.log(
-        'constructor(string memory name_: %s, string memory symbol_: %s)',
-        name_,
-        symbol_
-      );
+    modifier security {
+      require(msg.sender == _admin, 'Must be contract admin');
+      _;
     }
-    setAdmin(msg.sender);
-    // setTotalSupply(totalSupply_);
-    // mint(getAdmin(), totalSupply_);
-  }
 
-  function totalSupply() external view override returns (uint256) {
-    return _totalSupply;
-  }
+    modifier safe(address account) {
+      require(account != address(0), 'Cannot transact with the zero address');
+      _;
+    }
 
-  function setTotalSupply(uint256 totalSupply_) internal {
-    // require() // Ensure positive #
-    // Can uint256 have an overflow? If not, explain that next to variable declaration.
-    _totalSupply = totalSupply_;
-  }
+    modifier restricted(uint256 number) {
+      require(number != 0, 'Number cannot be zero');
+      require(number > 0, 'Must be a positive number.');
+      require(number <= _totalSupply, 'Must be less than total supply.');
+      require(
+        (_totalMinted + number) <= _totalSupply,
+        'This would cause the total minted coins to be more than the total supply.'
+      );
+      _;
+    }
 
-  function getTotalMinted() public view returns (uint256) {
-    return _totalMinted;
-  }
+    modifier reentrancyProtection() {
+      require(_status != _ENTERED, 'Reentrant call');
+      _status = _ENTERED;
+      _;
+      _status = _NOT_ENTERED;
+    }
 
-  function balanceOf(address account)
-    external
-    view
-    override
-    returns (uint256)
-  {
-    return _balances[account];
-  }
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) payable {
+        setAdmin(msg.sender);
+        // setTotalSupply(totalSupply_);
+        // mint(getAdmin(), totalSupply_);
+    }
 
-  function transfer(address recipient, uint256 amount) public override returns (bool){
-        _transfer(msg.sender, recipient, amount);
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return _balances[account];
+    }
+
+    function setTotalSupply(uint256 totalSupply_) internal {
+        _totalSupply = totalSupply_;
+    }
+
+    function getTotalMinted() public view returns (uint256) {
+        return _totalMinted;
+    }
+
+
+    function transfer(address recipient, uint256 amount) public override returns (bool){
+          _transfer(msg.sender, recipient, amount);
+          return true;
+    }
+
+    function approve(address spender, uint256 amount) external override returns (bool){
+        _approve(msg.sender, spender, amount);
         return true;
-  }
+    }
 
-function approve(address spender, uint256 amount) external override returns (bool){
-  _approve(msg.sender, spender, amount);
-  return true;
-}
-
-function _approve(address owner, address spender, uint256 amount) internal {
-  require(owner != address(0), "ERC20: approve from the zero address");
-  require(spender != address(0), "ERC20: approve to the zero address");
-  _allowances[owner][spender] = amount;
-  emit Approval(owner, spender, amount);
-}
-function _transfer(address sender, address recipient, uint256 amount) internal {
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+    function _transfer(address sender, address recipient, uint256 amount) internal {
 
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
@@ -133,67 +122,68 @@ function _transfer(address sender, address recipient, uint256 amount) internal {
         }
         _balances[recipient] += amount;
 
-        emit Transfer(sender, recipient, amount);}
-
-
-  function allowance(address owner, address spender)
-    external
-    view
-    override
-    returns (uint256)
-  {
-    return _allowances[owner][spender];
-  }
-
-  function transferFrom(
-    address spender,
-    address recipient,
-    uint256 amount
-  ) external override returns (bool) {
-    _transfer(spender, recipient, amount);
-
-  address sender = msg.sender;
-    uint256 currentAllowance = _allowances[spender][sender];
-    require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-    unchecked {
-        _approve(spender, sender, currentAllowance - amount);
+        emit Transfer(sender, recipient, amount);
     }
-    return true;
-  }
 
-  function name() external view returns (string memory) {
-    return _name;
-  }
 
-  function symbol() external view returns (string memory) {
-    return _symbol;
-  }
+    function allowance(address owner, address spender)
+      external
+      view
+      override
+      returns (uint256)
+    {
+      return _allowances[owner][spender];
+    }
 
-  function decimals() external pure returns (uint8) {
-    return TOKEN_DECIMALS;
-  }
+    function transferFrom(
+        address spender,
+        address recipient,
+        uint256 amount
+    ) external override returns (bool) {
+        _transfer(spender, recipient, amount);
 
-  function mint(address account, uint256 amount)
-    public
-    security
-    safe(account)
-    restricted(amount)
-  {
-    _balances[account] += amount;
-    emit Transfer(address(0), account, amount);
-  }
+       address sender = msg.sender;
+        uint256 currentAllowance = _allowances[spender][sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(spender, sender, currentAllowance - amount);
+        }
+        return true;
+    }
 
-  function getAdmin() public view returns (address) {
-    return _admin;
-  }
+    function name() external view returns (string memory) {
+        return _name;
+    }
 
-  function setAdmin(address admin) internal {
-    address _previousAdmin = _admin;
-    _admin = admin;
-    emit AdminChanged(_previousAdmin, _admin);
-  }
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
 
-  receive() external payable {
-    emit Fallback(msg.sender, msg.value);
-  }
+    function decimals() external pure returns (uint8) {
+        return TOKEN_DECIMALS;
+    }
+
+    function mint(address account, uint256 amount)
+        public
+        security
+        safe(account)
+        restricted(amount)
+    {
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function getAdmin() public view returns (address) {
+       return _admin;
+    }
+
+    function setAdmin(address admin) internal {
+        address _previousAdmin = _admin;
+        _admin = admin;
+        emit AdminChanged(_previousAdmin, _admin);
+    }
+
+    receive() external payable {
+        emit Fallback(msg.sender, msg.value);
+    }
 }
