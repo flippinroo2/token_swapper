29219d2d34dc7c01d3db15756b7c43096dbe43cf
Kevin L <flippinroo2@gmx.com>
2021-10-05 07:40:09 -0400

    Getting rid of unnecessary files.

--- avash/avash-healthcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -X POST --data '{
-  "jsonrpc":"2.0",
-  "id":1,
-  "method":"health.getLiveness"
-}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/health
--- avash/avash-staking-node.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/Users/kloy/go/pkg/mod/github.com/ava-labs/avash@v1.1.9/avash runscript /Users/kloy/go/pkg/mod/github.com/ava-labs/avash@v1.1.9/scripts/five_node_staking.lua
